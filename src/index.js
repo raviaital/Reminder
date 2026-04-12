56b6bf2a6ea2441f41d030e0f5d0e42693c981a4
Ravi Aital <raviaital@gmail.com>
2026-04-12 17:11:21 +0530

    Initial Setup for backend

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'; // Imports Express.js.
+
+const app = express() // Creates an Express application instance.
+const port = process.env.PORT
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+}) // Defines a route (/) that returns Hello, World!.
+
+// Listen to PORT
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}) // Starts the server on port 3000 and logs a confirmation message.
